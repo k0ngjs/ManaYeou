ec9d3057f61ce5c1b9620cd7f588502e0fd1460f
k0ngjs <kgstar123@naver.com>
2026-04-11 14:28:07 +0900

    chore: versionName 1.0.0 (최초 릴리즈)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 2
-        versionName = "1.2.0"
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
