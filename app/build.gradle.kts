1970188efc69cebbeee52d7f93eae72bb2ada66e
k0ngjs <kgstar123@naver.com>
2026-04-12 15:27:16 +0900

    chore: versionCode 3, versionName 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.fubuki.manarabbit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
